c414a21eb1bbf40fc22d340e809667e95f325d59
Emil Shafigin <shafigin@gmail.com>
2021-07-05 15:05:43 +0600

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by Emil Shafigin on 5/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
